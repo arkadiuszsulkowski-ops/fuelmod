340a57d526d27449289d1eeeead29bfb67f1184d
arkad <arkadiuszsulkowski@gmail.com>
2026-02-05 23:07:52 +0100

    v 5

--- src/main/java/pl/sula008/fuelmod/FuelCommands.java
+++ src/main/java/pl/sula008/fuelmod/FuelCommands.java
@@ -5,28 +5,31 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ConfigTracker;
 import net.minecraftforge.fml.config.ModConfig;
+import java.util.Optional;
 
-@Mod.EventBusSubscriber(modid = "fuelmod", bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class FuelCommands {
 
-    @SubscribeEvent
     public static void onRegisterCommands(RegisterCommandsEvent event) {
-        event.getDispatcher().register(Commands.literal("fuelmod")
-                .requires(source -> source.hasPermission(2))
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+
+        // Rejestrujemy komendę bez żadnych hasPermission i bez skomplikowanych builderów
+        dispatcher.register(Commands.literal("fuelmod")
                 .then(Commands.literal("reload")
                         .executes(context -> {
-                            ConfigTracker.INSTANCE.configSets().get(ModConfig.Type.COMMON)
-                                    .forEach(config -> config.getSpec().acceptConfig(config.getConfigData()));
+                            Optional<? extends ModConfig> config = ConfigTracker.INSTANCE.configSets()
+                                    .get(ModConfig.Type.COMMON)
+                                    .stream()
+                                    .filter(c -> c.getSpec() == FuelConfig.SPEC)
+                                    .findFirst();
 
-                            // Zmienione na Component.translatable
-                            context.getSource().sendSuccess(() ->
-                                    Component.translatable("commands.fuelmod.reload.success"), true);
-
-                            return 1;
+                            if (config.isPresent()) {
+                                FuelConfig.SPEC.acceptConfig(config.get().getConfigData());
+                                context.getSource().sendSuccess(() -> Component.literal("§aFuelMod reloaded!"), true);
+                                return 1;
+                            }
+                            return 0;
                         })
                 )
         );
